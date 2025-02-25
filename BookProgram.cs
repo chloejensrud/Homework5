06eff60ab2445b96739c759d7dc9f708e2744cda
chloejensrud <ij1247ov@go.minnstate.edu>
2025-02-24 18:47:48 -0600

    Add files via upload

---
+++ BookProgram.cs
@@ -0,0 +1,18 @@
+﻿// Written by Chloe Jensrud
+// 2/24/2025
+
+namespace LibrarySystem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            EBook aEbook = new EBook("Power", "Jeffery Pfeffer", 2011, 208);
+
+            PrintedBook aPrintedBook = new PrintedBook("Wind and Truth", "Brandon Sanderson", 2024, 1248);
+
+            Console.WriteLine(aEbook.ToString());
+            Console.WriteLine(aPrintedBook.ToString());
+        }
+    }
+}
---
+++ EBook.cs
@@ -0,0 +1,63 @@
+﻿// Written by Chloe Jensrud
+// 2/24/2025
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystem
+{ // EBook subclass
+    public class EBook : Book
+    { // class variables
+        private int fileSizeMB = 0;
+
+        //gets and sets
+        public int FileSizeMB
+        {
+            get { return fileSizeMB; }
+            set { fileSizeMB = value; }
+        }
+        //constructor calling base
+        public EBook(string title, string author, int yearPublished, int fileSizeMB) : base(title, author, yearPublished)
+        {
+            FileSizeMB = fileSizeMB;
+        }
+
+        //ToString
+        public override string ToString()
+        {
+            string msg = "";
+            msg += $"{Title} by {Author} {YearPublished} \nFile Size: {FileSizeMB}MB";
+            return msg;
+        }
+    }
+
+    // printed book subclass
+    public class PrintedBook : Book
+    { // class variables
+        private int pageCount = 0;
+
+        // gets and sets
+        public int PageCount
+        {
+            get { return pageCount; }
+            set { pageCount = value; }
+        }
+        // constructor calling base
+        public PrintedBook(string title, string author, int yearPublished, int pageCount) : base(title, author, yearPublished)
+        {
+            PageCount = pageCount;
+        }
+
+        // ToString
+        public override string ToString()
+        {
+            string msg = "";
+            msg += $"{Title} by {Author} {YearPublished} \nNumber of Pages: {PageCount}";
+            return msg;
+        }
+
+    }
+}
